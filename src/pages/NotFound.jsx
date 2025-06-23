e1cb8cf6d59d23f5d12a8d0a5fa89cea0602ffc4
Palen T <209441589+PalenT056@users.noreply.github.com>
2025-06-23 13:20:32 +0700

    update

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function NotFound () {
     return (
-        <div>
-            
+        <div className="flex flex-col h-screen items-center justify-center">
+            <p className="text-4xl font-bold">Not Found</p>
+            <Link to={`/`}>
+                <p className="text-blue-700 underline">Back</p>
+            </Link>
         </div>
     )
 }
